6cc6c58e59e8a1b5b2225face5bee49ae21f8f93
Nitish Kumar <iamnitishsah12@gmail.com>
2025-01-20 17:19:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ if st.button("Predict"):
 st.markdown(
     """
     ---
-    Made with ❤️ by [Nitish Sah](https://www.instagram.com/nitishadow/) just for youuuu.
+    Made with ❤️ by [Nitish Sah](https://www.instagram.com/nitishadow/) just for you.
     """
 )
